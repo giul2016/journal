0d2ee8ea5ecc35025e43a5a7ada1ff8d2e20ec85
mdamis <mdamis@users.noreply.github.com>
2016-02-03 14:27:27 +0100

    Add a menu to NewsContentActivity

--- app/src/main/java/fr/upem/journal/NewsContentActivity.java
+++ app/src/main/java/fr/upem/journal/NewsContentActivity.java
@@ -1,9 +1,14 @@
 package fr.upem.journal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class NewsContentActivity extends AppCompatActivity {
@@ -13,6 +18,7 @@ public class NewsContentActivity extends AppCompatActivity {
     private TextView descriptionTextView;
     private TextView pubDateTextView;
     private TextView sourceTextView;
+    NewsFeedItem newsFeedItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +27,42 @@ public class NewsContentActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        NewsFeedItem item = getIntent().getParcelableExtra("item");
+        newsFeedItem = getIntent().getParcelableExtra("item");
 
         titleTextView = (TextView) findViewById(R.id.itemTitle);
         descriptionTextView = (TextView) findViewById(R.id.itemDescription);
         pubDateTextView = (TextView) findViewById(R.id.itemPubDate);
         sourceTextView = (TextView) findViewById(R.id.itemSource);
 
-        titleTextView.setText(item.getTitle());
-        descriptionTextView.setText(Html.fromHtml(item.getDescription().replaceAll("<img.+/(img)*>", "")));
-        pubDateTextView.setText(item.getPubDate().toString());
-        sourceTextView.setText(item.getSource());
+        titleTextView.setText(newsFeedItem.getTitle());
+        descriptionTextView.setText(Html.fromHtml(newsFeedItem.getDescription().replaceAll("<img.+/(img)*>", "")));
+        pubDateTextView.setText(newsFeedItem.getPubDate().toString());
+        sourceTextView.setText(newsFeedItem.getSource());
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.news_content_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            case R.id.actionExplore:
+                Intent intent = new Intent(this, WebActivity.class);
+                intent.putExtra("link", newsFeedItem.getLink());
+                startActivity(intent);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
